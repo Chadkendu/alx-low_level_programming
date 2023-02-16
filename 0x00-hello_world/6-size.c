b53652d77f980f56d5f23ae30072b8c9a1790a32
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-16 09:54:04 -0800

    variables in c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,11 +17,11 @@ int main(void)
 	float floatType;
 
     // Sizeof operator is used to evaluate the size of a variable
-    printf("Size of char: %ld bytes\n",sizeof(charType));
-    printf("Size of int: %ld bytes\n",sizeof(integerType));
-    printf("Size of long int: %ld bytes\n",sizeof(longintType));
-    printf("Size of long long: %ld bytes\n",sizeof(longlongType));
-    printf("Size of float: %ld byte\n",sizeof(floatType));
+    printf("Size of a char: %ld bytes\n",sizeof(charType));
+    printf("Size of an int: %ld bytes\n",sizeof(integerType));
+    printf("Size of a long int: %ld bytes\n",sizeof(longintType));
+    printf("Size of a long long: %ld bytes\n",sizeof(longlongType));
+    printf("Size of a float: %ld byte\n",sizeof(floatType));
 
     return 0;
 }
