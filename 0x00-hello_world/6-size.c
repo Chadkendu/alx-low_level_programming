06434614077dd5f2936ce4b1d92f38b7cd8009ad
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-16 11:02:57 -0800

    c var

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,12 +10,6 @@
 
 int main(void)
 {
-	char charType;
-	int integerType;
-	long int longintType;
-	long long int longlongintType;
-	float floatType;
-
 	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
     	printf("Size of an int: %ld byte(s)\n", sizeof(integerType));
    	printf("Size of a long int: %ld byte(s)\n", sizeof(longintType));
