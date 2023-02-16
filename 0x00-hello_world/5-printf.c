68d724afe7582e8ec3ec4f44c03a936c1d584633
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-16 07:37:31 -0800

    C program printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ * main: Entry point
+ *
+ * Description: 'main is the entry point of the c program'
+ *
+ * Result: Always 0 (success)
+ *
+ * */
+
+int main(void)
+
+{
+	printf("\with proper grammar, but the outcome is a piece of art,\"\n);
+
+	return (0);
+}
