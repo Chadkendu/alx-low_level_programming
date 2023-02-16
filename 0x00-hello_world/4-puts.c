3e3176e9e2ffab4955c811dd54acd7c8a23ea978
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-16 06:44:48 -0800

    c prog

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
+/*main function*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
