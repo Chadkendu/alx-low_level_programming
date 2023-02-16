ea385b63e1187abd3467be6e17b2d00efdb97409
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-16 12:43:05 -0800

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,8 @@
  * Return: Always 1 (success)
  */
 
-int main()
+int main(void)
 {
-	write("and that piece of art is useful" - Dora Korpar, 2015-10-19"\n);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return(1);	
 }
