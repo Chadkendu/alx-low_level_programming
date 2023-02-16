c282692d1ec71baf813f94e0eaffcb30ddd68537
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-16 09:36:07 -0800

    variable types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /*
  * main - Entry point
  *
@@ -7,16 +8,18 @@
  * Return: Always 0 (success)
  */
 
-int main()
+int main(void)
 {	
     int integerType;
     float floatType;
+    long longType;
     double doubleType;
     char charType;
 
     // Sizeof operator is used to evaluate the size of a variable
     printf("Size of int: %ld bytes\n",sizeof(integerType));
     printf("Size of float: %ld bytes\n",sizeof(floatType));
+    printf("Size of long: %ld bytes\n",sizeof(longType));
     printf("Size of double: %ld bytes\n",sizeof(doubleType));
     printf("Size of char: %ld byte\n",sizeof(charType));
 
