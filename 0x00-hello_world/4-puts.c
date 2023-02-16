78738ab0d5d5e33d07853ce5393471e4fdad347b
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-16 06:14:51 -0800

    first program in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
 
-/*
- * main function is a integer function
- * it is used as an entry point for all C program
- * */
-
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle);
-
 	return(0);
 }
