398953f7b9f75b55ba938fad0204e82722516687
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-16 11:58:49 -0800

    c program

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/*
+ * main - Entry point
+ *
+ * Description: 'main is the entry point for all c programs'
+ *
+ * Return: Always 1 (success)
+ */
+
+int main()
+{
+	write("and that piece of art is useful" - Dora Korpar, 2015-10-19"\n);
+	return(1);	
+}
