96d83f10ae7b4c2fab665e8b2161b5a51deeba9b
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-17 14:39:30 -0800

    numbers with colon

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * Description: a program that prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int low;
+
+	for (low = '0,'; low <= '9'; low++)
+	{
+		putchar(low);
+
+		if (d != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
