1cd476f0a44188baef1b0add766a0bebae67e141
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-17 08:04:52 -0800

    putchar all alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: main is the entry point for all c program
+ * we are printing all the alphabet in lowercase
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char alphabet[] = ("a..z");
+	putchar("%s\n", alphabet);
+	return (0);
+}	
