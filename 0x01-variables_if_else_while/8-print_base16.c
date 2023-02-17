75aae5f4ebaf23e539cba35cedad7188c3b6847a
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-17 14:20:49 -0800

    done

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,12 +5,15 @@
 int main(void)
 {
 	int low;
-	char test;
 
-	for (low = 0; low <= 9; low++)
+	for (low = '0'; low <= '9'; low++)
+	{
 		putchar(low);
-	for (test = 'a'; test <= 'f'; test++)
-		putchar(test);
+	}
+	for (low = 'a'; low <= 'f'; low++)
+	{
+		putchar(low);
+	}
 	putchar('\n');
 	return (0);
 }
