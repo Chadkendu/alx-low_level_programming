bfbe4fb51c24624e4d9d7a1eca34ffefec723094
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-17 05:29:40 -0800

    srand

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 
-/* main - entry point
+/* main - 0-positive_or_negative.c
  *
  * Description: This is the code for random numbers generating using the seed rand keyword(srand) and rand
  *
@@ -15,11 +15,14 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n > 0)
+	if(n > 0) {
 		printf("%d is positive\n", n);
-	else if(n = 0)
+	}
+	else if(n = 0){
 		printf("%d is zero\n", n);
-	else(n < 0)
+	}
+	else(n < 0){
 		printf("%d is negative\n", n);
+	}
 	return (0);
 }
