20f650e63c81d986027144c1e644de395462a02f
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-17 09:50:01 -0800

    alphaBET lowercase and uppercase code

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * Description: code that print object in lowercase and uppercase
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char low;
+	char high;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+	for (high = 'A'; high <= 'Z'; high++)
+		putchar(high);
+	printf("\n");
+	return (0);
+}	
