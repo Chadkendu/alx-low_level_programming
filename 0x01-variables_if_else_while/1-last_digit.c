6147ef2a775d7504c7314c7dff74c4abe63da0ed
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-17 07:11:25 -0800

    srand last digit task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@
 int main(void)
 {
 	int n;
-	
+	char str[] = "Last digit of";	
 	lastDigit = n % 10;
 	
 	srand(time(0));
@@ -22,13 +22,13 @@ int main(void)
 	
 	if (lastDigit > 5)
 	{
-		printf("Last digit of %i is %i and is greater than 5\n", n,lastDigit);
+		printf("%s %d is %d and is greater than 5\n", str,n,lastDigit);
 	else if (lastDigit == 0)
 	{
-		printf("Last digit of %i is %i and is 0\n", n,lastDigit);
-	else (lastDigit < 6 ! 0)
+		printf("%s %d is %d and is 0\n", str,n,lastDigit);
+	else (lastDigit < 6)
 	{
-		printf("Last digit of %i is %i and is less than 6 and not 0\n", n,lastDigit);
+		printf("%s %d is %d and is less than 6 and not 0\n", str,n,lastDigit);
 	return (0);
 }
 
