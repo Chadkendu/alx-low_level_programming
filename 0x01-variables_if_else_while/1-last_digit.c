2b9354d6f007a8b4a7d6d49e2ed4289d026aa222
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-17 07:01:50 -0800

    srand of last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - 1-last_digit.c
+ *
+ * Description: srand for random numbers
+ *
+ * return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	lastDigit = n % 10;
+	
+	if (lastDigit > 5)
+	{
+		printf("Last digit of %i is %i and is greater than 5\n", n,lastDigit);
+	else if (lastDigit == 0)
+	{
+		printf("Last digit of %i is %i and is 0\n", n,lastDigit);
+	else (lastDigit < 6 ! 0)
+	{
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n,lastDigit);
+	return (0);
+}
+
