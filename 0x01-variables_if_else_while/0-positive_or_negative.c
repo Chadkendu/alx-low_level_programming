f7bdceda509287541866303841620362681241ee
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-17 05:22:04 -0800

    srand project

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /* main - entry point
  *
@@ -18,7 +19,7 @@ int main(void)
 		printf("%d is positive\n", n);
 	else if(n = 0)
 		printf("%d is zero\n", n);
-	else if(n < 0)
+	else(n < 0)
 		printf("%d is negative\n", n);
 	return (0);
 }
