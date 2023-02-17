9c0f914968c3fc0ace4561f7ee037ac1e635b8a2
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-17 05:11:04 -0800

    Srand c programming

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,10 +15,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if(n > 0)
-		printf("is positive\n");
-	if(n = 0)
-		printf("is zero\n");
-	if(n < 0)
-		printf("is negative");
+		printf("%d is positive\n", n);
+	else if(n = 0)
+		printf("%d is zero\n", n);
+	else if(n < 0)
+		printf("%d is negative\n", n);
 	return (0);
 }
