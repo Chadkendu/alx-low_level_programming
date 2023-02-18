201953c5eee461b0f070a217027920cf58e93650
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-17 23:43:03 -0800

    mix number

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+i#include <stdio.h>
+/**
+ * main-program entry point
+ * Return: 0 success, non zero fail
+ */
+int main(void)
+{
+	int i, k = '0', j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = k; j <= '9'; j++)
+		{
+			if (i != j)
+			{
+				putchar(i);
+				putchar(j);
+			}
+			if (i == j)
+			{
+				continue;
+			}
+			if (i == '8' && j == '9')
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		k++;
+	}
+	putchar('\n');
+	return (0);
+}
+
