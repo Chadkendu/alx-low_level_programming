826d19d7909393178f437b3714062090de9414e8
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-21 11:31:43 -0800

    Parv-4a: Modified (comment)

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,8 +4,9 @@
  * islower - checks for a lower case character
  *
  * Description: a function that checks for lowercase character.
- *
- * Return: Always 0 (success)
+ *@c : character to check the case
+
+ * Return: Always 0 or 1 (success)
  *
  */
 
