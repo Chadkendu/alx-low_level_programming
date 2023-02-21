1e7464cde7842fb2e91155fe3089927298758302
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-21 10:33:08 -0800

    Parv-2aviii: Modified (main.h)

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,7 +6,7 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
-int print_sign(int n):
+int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
