7c2c0d37331da4ca6dbe168a709996190734d5cf
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-21 11:27:41 -0800

    Parv-4: function that checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * islower - checks for a lower case character
+ *
+ * Description: a function that checks for lowercase character.
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
