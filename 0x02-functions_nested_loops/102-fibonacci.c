52507887ded5bf45fc1d33b69f183cee6d949c6c
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-21 13:02:44 -0800

    Parv-15: first 50 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ *
+ * Description: computes and prints the sum of all the multiples of 3 or
+ * 5 below 1024 (excluded), followed by a new line
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int i = 0;
+	long int a = 0, b = 1, next;
+
+	while (i < 50)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+		printf("%lu", next);
+
+		if (i < 49)
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
