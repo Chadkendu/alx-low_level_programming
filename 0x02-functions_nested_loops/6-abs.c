95fecd5def9ae7b24d14990449b4cc095bf7ec8b
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-21 12:32:50 -0800

    Parv-7: a function that computes the absolute value of an integer.

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Checks for the absolute value
+ *
+ * Description: a function that computes the absolute value of an integer.
+ *
+ * @n : number to check
+ *
+ * Return: Always 0 or 1 (success)
+ *
+ */
+
+int _abs(int)
+{
+	return (n >= 0 ? n : n * -1);
+}
