0650a98392886438923efc6f39e9679a84aa3477
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-21 13:29:29 -0800

    Parv-11: function that adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - returns the sum of its parameters
+ *
+ * @a: int type number
+ *
+ * @b: int type number
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
