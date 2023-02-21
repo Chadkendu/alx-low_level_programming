2ce4acb4620fc3cd73ef5e5eb51d6959842dcdd1
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-21 10:35:26 -0800

    Parv-2aix: Modified (comment)

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,8 @@
 /**
  * print_alphabet - Entry point
  *
- * Description: a function that prints the alphabet, in lowercase, followed by a new line.
+ * Description: a function that prints the alphabet, in lowercase,
+ * followed by a new line.
  *
  * Return: Always 0 (success)
  *
