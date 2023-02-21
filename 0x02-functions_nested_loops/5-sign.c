5cbaff91190b40cb31e447dcc45b3fa6b1b35d1b
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-21 12:19:14 -0800

    Parv-6: a function that prints the sign of a number.

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _print_sign - prints the sign of a number
+ *
+ * Description: a function that prints the sign of a number.
+ *
+ * @c : character to check for a number
+ *
+ * Return: Always 0 or 1 (success)
+ *
+ */
+
+int print_sign(int n)
+{
+	int parv;
+
+	if (n > 0)
+	{
+		parv = 1;
+		_putchar ('+');
+	}
+	else if (n < 0)
+	{
+		parv = 0;
+		_putchar ('0');
+	}
+	else
+	{
+		parv = -1;
+		_putchar ('-');
+	}
+	return (parv);
+}
