6db0f98b6c05bede33c2fe52968086d5144dbfca
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-21 08:12:34 -0800

    Parv-2: Alphabet in lowercase c project

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Write a function that prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	for (a == 'a'; a <= 'z'; a++)
+	{
+		int a;
+		_putchar (a);
+		_putchar ('\n');
+	}
+	return (0);
+}
