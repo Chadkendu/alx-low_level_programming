89287087fd7de2655f3fc47dab1375d4357b6acd
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-21 12:56:56 -0800

    Parv-16: Fibonacci sequence whose values do not exceed 4,000,000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ *
+ * Description: computes and prints even  number < 4,000,000
+ * 5 below 1024 (excluded), followed by a new line
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int a = 0, b = 1, next = 0;
+	int sum = 0;
+
+	while (next < 4000000)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+		if (next % 2 == 0)
+			sum += next;
+	}
+	printf("%i\n", sum);
+	return (0);
+}
