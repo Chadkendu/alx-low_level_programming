fc3300d50109f59e71af5bfb6a32e76448924761
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-21 13:07:27 -0800

    Parv-14: sum of all the multiples of 3 or 5 below 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main block
+ *
+ * Description: computes and prints the sum of all the multiples of 3 or
+ * 5 below 1024 (excluded), followed by a new line
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int c = 0;
+	int sum = 0;
+
+	while (c < 1024)
+	{
+		if (c % 3 == 0 || c % 5 == 0)
+		{
+			sum += c;
+		}
+
+		c++;
+	}
+	printf("%i\n", sum);
+	return (0);
+}
