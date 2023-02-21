1388e4fb08f8acc456cf9d104516811e3628c5e3
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-21 05:04:57 -0800

    Parv1: Print a new line (task 0) c function project.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Write a program that prints _putchar, followed by a new line.
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	putchar ('_');
+	putchar ('p');
+	putchar ('u');
+	putchar ('t');
+	putchar ('c');
+	putchar ('h');
+	putchar ('a');
+	putchar ('r');
+	putchar ('\n');
+	
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Alx project on Functions, nested loops
