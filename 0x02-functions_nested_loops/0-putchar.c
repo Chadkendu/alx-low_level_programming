ca40b4602599af72633b2c91eea8821ceadcfd4d
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-21 05:15:43 -0800

    Parv-1aiii: Modified (main.h)

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,26 +2,22 @@
 #include "main.h"
 
 /**
- * main - entry point
- *
- * Description: Write a program that prints _putchar, followed by a new line.
- *
- * Return: Always 0 (success)
- *
+ * main - Entry point.
+ * Description: write a program that prints "_putchar" followed by a new line.
+ * Return: Always 0 (Success.)
  */
 
 int main(void)
 {
-	_putchar ('_');
-	_putchar ('p');
-	_putchar ('u');
-	_putchar ('t');
-	_putchar ('c');
-	_putchar ('h');
-	_putchar ('a');
-	_putchar ('r');
-	_putchar ('\n');
-	
-	return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
+	return (0);
 }
