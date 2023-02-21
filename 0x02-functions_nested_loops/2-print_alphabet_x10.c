1ea9349363bf18a943147dad4e66c3d312a05a21
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-21 11:14:26 -0800

    Parv-3: Alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: a function that prints 10 times the alphabet, in lowercase,
+ * followed by a new line.
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char p;
+	int i = 0;
+
+	while (i < 10)
+	{
+		p = 'a';
+
+		while (p <= 'z')
+		{
+			_putchar(p);
+			p++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}	
