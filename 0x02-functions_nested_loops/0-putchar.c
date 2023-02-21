d0169a40ca384b1c138fb56189ef4dc4382e730b
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-21 05:11:10 -0800

    Parv-1aii: Modified (main.h)

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,15 +12,15 @@
 
 int main(void)
 {
-	putchar ('_');
-	putchar ('p');
-	putchar ('u');
-	putchar ('t');
-	putchar ('c');
-	putchar ('h');
-	putchar ('a');
-	putchar ('r');
-	putchar ('\n');
+	_putchar ('_');
+	_putchar ('p');
+	_putchar ('u');
+	_putchar ('t');
+	_putchar ('c');
+	_putchar ('h');
+	_putchar ('a');
+	_putchar ('r');
+	_putchar ('\n');
 	
 	return (0);
 
