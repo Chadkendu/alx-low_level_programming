8519849fe521889a3b6b045b753595e9b740cc3b
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-21 12:45:02 -0800

    Parv-8: a function that prints the last digit of a number.

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "math.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number
+ *
+ * Description: a function that prints the last digit of a number.
+ *
+ * @n : number to check
+ *
+ * Return: Always 0 or 1 (success)
+ *
+ */
+
+int print_last_digit(int n)
+{
+	int nv;
+
+	if (n < 0)
+		nv = -1 * (n % 10);
+	else
+		nv = n % 10;
+
+	_putchar(nv + '0');
+		return (nv);
+}
