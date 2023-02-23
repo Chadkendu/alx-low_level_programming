b311e20ce40c50b8339f45d4a1559d6a46a17f8e
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-23 12:44:29 -0800

    Parv-6: Print 0-14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints the range 0-14 ten times.
+ *
+ * Return: 0.
+ *
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	i = j = 0;
+	while (i < 10)
+	{
+		while (j <= 14)
+		{
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			++j;
+		}
+		_putchar('\n');
+		j = 0;
+		i++;
+	}
+}
