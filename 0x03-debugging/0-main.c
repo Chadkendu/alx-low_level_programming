a01265f966e799e6eeb7a1532a960bfea24c71ef
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-23 07:01:05 -0800

    Parv-1a: Modified syntax

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
+ * main - test function that prints if integer is positive or negative
+ * 
+ * Return: 0
+ *
+ */
 
 int main(void)
 {
-        int i;
+	int i;
 
-        i = 0;
-        positive_or_negative(i);
+	i = 98;
 
-        return (0);
-}
+	positive_or_negative(i);
+
+	return (0);
+}	
