fba13e70e68fe010c20bdaaa0869e42258246b28
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-23 12:01:21 -0800

    Parv-3a: Modified syntax

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * mul - multiplies a number
+ * mul - multiplies two numbers a and b
  *
- * Description: Write a function that multiplies two integers.
+ * Description: Write a function that multiplies two integers a and b.
  * @c: character to check
  *
  * Return: Always 0 (success)
@@ -12,5 +12,5 @@
 
 int mul(int a, int b)
 {
-	return (a*b);
+	return (a * b);
 }
