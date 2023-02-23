e7b193d3688bd4930991b1076bf874706305df16
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-23 11:23:18 -0800

    Parv-1aiii: Modified main.h add _putchar

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int i_supper(int c);
-int is_digit(int c);
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
