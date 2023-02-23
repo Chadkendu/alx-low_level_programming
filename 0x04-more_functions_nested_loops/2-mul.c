3f768262dc06f0dca66ba0077e11dce5962bfb74
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-23 11:58:05 -0800

    Parv-3: Multiplt two digit (mul)

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies a number
+ *
+ * Description: Write a function that multiplies two integers.
+ * @c: character to check
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int mul(int a, int b)
+{
+	return (a*b);
+}
