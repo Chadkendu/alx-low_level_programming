808108f7cc331e9b7fdd120cf73c321cc60ba9cb
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-23 13:57:36 -0800

    Parv-12: prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints out the larget prime factor of 612852475143
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	long i;
+	long num = 612852475143;
+
+	for (i = 2; i < num; i++)
+	{
+		if (num % i == 0)
+			num /= i;
+	}
+	printf("%li\n", num);
+	return (0);
+}
