71b45aaa93ef3f4cde43eb7fdae60bf8a3e499f1
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-23 12:10:17 -0800

    Parv-4: print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_numbers - displays numbers to the output
+ *
+ * Description: Write a function that prints the numbers, from 0 to 9,
+ * followed by a new line.
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+void print_numbers(void)
+{
+	while (a = 0)
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+	return (0);
+}
