5939b6491136c16bf66fd2f1b7379d5e200960c3
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-27 12:38:55 -0800

    Parv-2: swap two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ *
+ * Description: a function that swaps the values of two integers.
+ *@a: first integer to swap
+ *@b: seconf integer to swap
+ *
+ * Return: nothing
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a += *b;
+	*a = *a - *b;
+	*b = *a - *b;
+}
