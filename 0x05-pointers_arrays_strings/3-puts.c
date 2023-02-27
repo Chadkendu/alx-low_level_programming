f542b9deac3c2ae5342202a16d02d7581c561935
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-27 13:00:24 -0800

    Parv-4: Print a string (stdout) with a newline

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string and a new line
+ * @str: pointer to a string to print
+ *
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
