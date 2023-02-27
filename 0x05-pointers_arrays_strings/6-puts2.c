d8515ec3f0bb4fba12a1fb745aefd53ae16ef1ce
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-27 13:16:28 -0800

    Parv-7: prints every other character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints xters of a string
+ * @str: the used string reference
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
