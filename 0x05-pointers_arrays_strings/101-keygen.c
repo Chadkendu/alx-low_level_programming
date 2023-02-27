86e0ca92a2a42a8a06ad9163e1b2e43fe8e1dba3
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-27 13:36:13 -0800

    Parv-12:  a program that generates random valid passwords for the program

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random passwords for 101-crackme
+ * Return: zero
+ */
+
+int main(void)
+{
+	int sum;
+	char c;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
