59d372a83915b4bf09f563531b526b165339acbd
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-27 11:51:23 -0800

    Parv-1aiii: Modified main.c (removed merge syntax)

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -1,10 +1,7 @@
 #ifndef main_h
 #define main_h
 
-<<<<<<< HEAD
-=======
 int _putchar(char c);
->>>>>>> 69ff44cb94e487711fc88165bcf32593df1c4d98
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
