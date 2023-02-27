c27a31b1c599b127466180b89524e3fbb20eed2f
Joshua Ajayi <kenduajayi@gmail.com>
2023-02-27 13:08:36 -0800

    Parv-6: reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - prints a string in reverse
+ * @s: the used string to beb reversed
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+	int len, i, half;
+	char temp;
+
+	for (len = 0; s[len] != '\0'; len++)
+	;
+	i = 0;
+	half = len / 2;
+
+	while (half--)
+	{
+		temp = s[len - i - 1];
+		s[len - i - 1] = s[i];
+		s[i] = temp;
+		i++;
+	}
+}
