a540b7b94deadaaf672093cabef79d750e4134d0
Joshua Ajayi <kenduajayi@gmail.com>
2023-03-14 14:34:17 -0700

    Parv-2: a function that concatenates two strings.

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - Concatenates two strings.
+ * @s1: The string to be concatenated upon
+ * @s2: The string to be concatenated to s1
+ *
+ * Return: If concatenation fails - NULL.
+ * otherwise - a pointer the newly - allocted space in memory
+ * containing the concatenated strings.
+ *
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *concat_str;
+	int index, concat_index = 0, len = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (index = 0; s1[index] || s2[index]; index++)
+		len++;
+
+	concat_str = malloc(sizeof(char) * len);
+
+	if (concat_str == NULL)
+		return (NULL);
+
+	for (index = 0; s1[index]; index++)
+		concat_str[concat_index++] = s1[index];
+
+	for (index = 0; s2[index]; index++)
+		concat_str[concat_index++] = s2[index];
+
+	return (concat_str);
+}
+
