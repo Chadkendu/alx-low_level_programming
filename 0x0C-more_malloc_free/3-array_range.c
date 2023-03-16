567835d8c21c6f2e54707f51d0f717d2cd1328c2
Joshua Ajayi <kenduajayi@gmail.com>
2023-03-16 15:03:05 -0700

    Parv-3: a function that creates an array of integers.

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * array_range - Creates an array of integers ordered from
+ * mim to max, inclusive
+ * @min: The first value of the array
+ * @max: The last value of the array
+ *
+ * Return: If min > max or the function fails - NULL
+ * Otherwise - a pointer to the newly created array
+ *
+ */
+
+int *array_range(int min, int max)
+{
+	int *array, index, size;
+
+	if (min > max)
+		return (NULL);
+
+	size = max - min + 1;
+
+	array = malloc(sizeof(int) * size);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+		array[index] = min++;
+
+	return (array);
+}
