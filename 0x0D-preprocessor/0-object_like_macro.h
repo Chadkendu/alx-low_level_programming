36f5613dcabdab9476b95d7fa8fb3ead14b5407b
Joshua Ajayi <kenduajayi@gmail.com>
2023-03-20 14:44:00 -0700

    Parv: a header file that defines a macro named SIZE as an abbreviation for the token 1024.

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * File: 0-object_like_macro.h
+ * Desc: Header file that defines a macro named
+ * SIZE as an abbreviation for the token 1024
+ */
+
+#define SIZE 1024
+
+#endif
