526223f11376222b1e1884c50ec3535b8ee6a001
Joshua Ajayi <kenduajayi@gmail.com>
2023-03-20 14:19:23 -0700

    Parv-1a: Modified ( a function that initialize a variable of type struct dog)

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,7 +6,7 @@
  * @d: The dog to be initialized.
  * @name: The name of the dog
  * @age: The age of the dog
- * @owner: The owner of the dog
+ * @owner: The owner of the dog.
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
