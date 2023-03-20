f90bfcf3c73b722ec97c0f321152c8cd85ad1cbe
Joshua Ajayi <kenduajayi@gmail.com>
2023-03-20 14:50:44 -0700

    Parv-4: a function-like macro SUM(x, y) that computes the sum of the numbers x and y.

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * File - a macro that adds two integers
+ * Desc: Add two integer
+ * @x: first integer
+ * @y: second integer
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
