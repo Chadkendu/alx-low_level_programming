ac5e5a2dfefd2acf47893526eae3e7f0ae683bfb
Joshua Ajayi <kenduajayi@gmail.com>
2023-03-30 16:41:40 -0700

    Parv7: a 64-bit program in assembly that prints Hello, Holberton, followed by a new line.

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,27 @@
+; My comments: It is so fun to write in assembly language
+; File: 101-hello_holberton.asm
+; Auth: Stephen Oloo
+; Desc: 64-bit assembly program that prints
+;       Hello, Holberton followed by a new line.
+
+extern printf
+
+section .text
+   global main
+
+main:
+   push rbp
+
+   mov rdi,fmt
+   mov rsi,msg
+   mov rax,0
+   call printf
+
+   pop rbp
+
+   mov rax,0
+   ret
+
+section .data
+   msg: db "Hello, Holberton", 0
+   fmt: db "%s", 10, 0
