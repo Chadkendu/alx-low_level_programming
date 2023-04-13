23ad803dd9f47d75aedd8c0ddee1851850d4ac6f
Joshua Ajayi <kenduajayi@gmail.com>
2023-04-13 07:55:30 -0700

    Parv: Modifies syntax

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,35 +1,40 @@
 #include "main.h"
-#include <stddef.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /**
- * read_textfile - reads a text file and prints it to the standard output
- * @filename: name of the file
- * @letters:  number of letters to be printed
+ * read_textfile - A text file and prints it to POSIX stdout.
+ * @filename: pointer to the name of the file.
+ * @letters: Number of letters the
+ *           function should read and print.
  *
- * Return: number of letters read and printed
+ * Return: number of letters read and printed, or 0 on failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file, n_read, wrote;
+	ssize_t fd, rd_cnt, wr_count;
 	char *buffer;
 
-	buffer = malloc(sizeof(*buffer) * (letters + 1));
-	if (filename == NULL || buffer == NULL)
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	rd_cnt = read(fd, buffer, letters);
+	wr_count = write(STDOUT_FILENO, buffer, rd_cnt);
+
+	if (fd == -1 || rd_cnt == -1 || wr_count == -1 || wr_count != rd_cnt)
 	{
 		free(buffer);
 		return (0);
 	}
-	file = open(filename, O_RDONLY);
-	if (file == -1)
-		return (0);
-	n_read = read(file, buffer, letters);
-	if (n_read == -1)
-		return (0);
-	buffer[n_read] = '\0';
-	wrote = write(STDOUT_FILENO, buffer, n_read);
-	if (wrote != n_read)
-		return (0);
+
 	free(buffer);
-	close(file);
-	return (n_read);
+	close(fd);
+
+	return (wr_count);
 }
