2012bca5352b0d6d819316c49e886dba3feecdd4
Joshua Ajayi <kenduajayi@gmail.com>
2023-04-12 21:00:39 -0700

    Parv1: a function that creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - create a file with read/write access for user
+ * @filename: name of file to create
+ * @text_content: string to write to file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+int fd, rstatus, i;
+
+if (filename == NULL)
+	return (-1);
+
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+
+if (fd == -1)
+	return (-1);
+
+if (text_content)
+{
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
+	rstatus = write(fd, text_content, i);
+	if (rstatus == -1)
+		return (-1);
+}
+
+close(fd);
+return (1);
+}
