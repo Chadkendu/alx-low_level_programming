c0aef9d36684f3f7cdfe053d45479064a6617131
Joshua Ajayi <kenduajayi@gmail.com>
2023-04-14 02:33:34 -0700

    Parv2: Modified syntax

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
+ * Auth: Joshua Ajayi
  * append_text_to_file - Append text to the end of a file
  * @filename: name of file
  * @text_content: string to add to end of file
  * Return: 1 on success, -1 on failure
  */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, status, i;
