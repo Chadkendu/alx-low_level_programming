7730e6529c9650495e37ac5868087a0dd83f9743
Joshua Ajayi <kenduajayi@gmail.com>
2023-04-14 02:15:25 -0700

    Parv6: Modified syntax

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,7 +2,8 @@
 
 /**
  * get_endianness - checks the endianness
- * Return: 0 if big endian, 1 if little endian
+ * 
+ * Return: 0 if big endian, and 1 if little endian
  */
 
 int get_endianness(void)
