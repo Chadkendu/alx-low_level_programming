cca5b72d6095e19018ac53fd26db2afc5fbf655e
Joshua Ajayi <kenduajayi@gmail.com>
2023-04-14 02:13:51 -0700

    Parv: Modified syntax

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,8 +2,10 @@
 
 /**
  * binary_to_uint - converts a binary number to unsigned int
+ *
  * @b: string containing the binary number
- * Return: the converted number
+ * 
+ * Return: converted number
  */
 
 unsigned int binary_to_uint(const char *b)
