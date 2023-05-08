e5ebff4ae4f3f07b77ae4884a19d96fcdd42b018
Joshua <kenduajayi@gmail.com>
2023-05-08 07:38:17 -0700

    new file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fiel = open(filename, O_WRONLY | O_APPEND);
+	file = open(filename, O_WRONLY | O_APPEND);
 	if (file < -1)
 		return (-1);
 
@@ -33,5 +33,6 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	close(file);
+
 	return (1);
 }
