e0059c8bedbd53271ef56be1632773cd72eca136
Joshua <kenduajayi@gmail.com>
2023-05-08 07:22:50 -0700

    new file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,40 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
  * Auth: Joshua Ajayi
  * create_file - create a file with read/write access for user
- * @filename: name of file to create
+ *
+ * Description:
+ * @filename: file name to create
  * @text_content: string to write to file
+ *
  * Return: 1 on success, -1 on failure
  */
 
 int create_file(const char *filename, char *text_content)
 {
-int fd, rstatus, i;
-
-if (filename == NULL)
-	return (-1);
-
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-
-if (fd == -1)
-	return (-1);
-
-if (text_content)
-{
-	for (i = 0; text_content[i] != '\0'; i++)
-		;
-	rstatus = write(fd, text_content, i);
-	if (rstatus == -1)
+	int file, Bytestat, length = 0;
+	
+	if (filename == NULL)
 		return (-1);
-}
+	
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file == -1)
+		return (-1);
+	
+	if (text_content != NULL)
+	{
+		while (*(text_content + length) != '\0')
+			length++;
+	}
 
-close(fd);
-return (1);
+	Bytestat = write(file, text_content, length);
+	if (Bytestat == -1)
+	{
+		close(file);
+		return (-1);
+	}
+	close(file);
+	return (1);
 }
