8afc6c2506f71fdf107cb37520d3a674d185842e
Joshua Ajayi <kenduajayi@gmail.com>
2023-10-11 00:21:55 -0700

    function that searches for a value in a sorted list of integers using the Jump search algorithm.

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,54 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_list - function that searches for value in an array of
+ * integers using Jump search algorithm
+ *
+ * Description:
+ * @list: the input list
+ * @size: size of array
+ * @value: the value to search in
+ * Return: index of number
+ */
+
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	size_t index, k, m;
+	listint_t *prev;
+
+	if (list == NULL || size == 0)
+		return (NULL);
+
+	m = (size_t)sqrt((double)size);
+	index = 0;
+	k = 0;
+
+	do {
+		prev = list;
+		k++;
+		index = k * m;
+
+		while (list->next && list->index < index)
+			list = list->next;
+
+		if (list->next == NULL && index != list->index)
+			index = list->index;
+
+		printf("Value checked at index [%d] = [%d]\n", (int)index, list->n);
+
+	} while (index < size && list->next && list->n < value);
+
+	printf("Value found between indexes ");
+	printf("[%d] and [%d]\n", (int)prev->index, (int)list->index);
+
+	for (; prev && prev->index <= list->index; prev = prev->next)
+	{
+		printf("Value checked at index [%d] = [%d]\n", (int)prev->index, prev->n);
+		if (prev->n == value)
+			return (prev);
+	}
+
+	return (NULL);
+}
+
