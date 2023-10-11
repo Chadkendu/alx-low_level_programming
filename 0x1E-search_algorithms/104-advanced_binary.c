45c3688629f0644d512264b21db2f5847eabf56b
Joshua Ajayi <kenduajayi@gmail.com>
2023-10-11 00:19:46 -0700

    a function that searches for a value in a sorted array of integers.

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,67 @@
+#include "search_algos.h"
+
+/**
+ * rec_search - function that searches for a value in an array of
+ * integers using Binary search algorithm
+ *
+ * Description:
+ * @array: the input array
+ * @size: size of array
+ * @value: the value to search in
+ * Return: index of number
+ */
+
+int rec_search(int *array, size_t size, int value)
+{
+	size_t half = size / 2;
+	size_t i;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	printf("Searching in array");
+
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
+	printf("\n");
+
+	if (half && size % 2 == 0)
+		half--;
+
+	if (value == array[half])
+	{
+		if (half > 0)
+			return (rec_search(array, half + 1, value));
+		return ((int)half);
+	}
+
+	if (value < array[half])
+		return (rec_search(array, half + 1, value));
+
+	half++;
+	return (rec_search(array + half, size - half, value) + half);
+}
+
+/**
+ * advanced_binary - function that calls to rec_search to return
+ * index of number
+ *
+ * Description:
+ * @array: the input array
+ * @size: size of array
+ * @value: the value to search in
+ * Return: index of number
+ */
+
+int advanced_binary(int *array, size_t size, int value)
+{
+	int index;
+
+	index = rec_search(array, size, value);
+
+	if (index >= 0 && array[index] != value)
+		return (-1);
+
+	return (index);
+}
